58a917d569c2c2f2d580c6fcbada8387c326a051
bilu-Blen <azblen1@gmail.com>
2018-09-18 17:02:54 -0400

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,49 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+//    first app
+/**/
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+
+
+
+        /*for(int count=0; count <10; count++)
+        {
+
+            if(count==5)
+            {
+                System.out.println("Wohoooooo");
+            }
+            else {
+                System.out.println(count);
+            }
+        }
+        */
+        /*int count =1;
+        do{
+            if(count==5)
+            {
+                System.out.println("Wohoooooo");
+            }
+            else {
+                System.out.println(count);
+            }
+            count++;
+        }while(count<=10);*/
+
+        int count = 0;
+        while (count<=10){
+            if(count==5)
+            {
+                System.out.println("Wooohooo");
+            }else{
+                System.out.println(count);
+
+            }
+            count ++;
+        }
+
+    }
+}
